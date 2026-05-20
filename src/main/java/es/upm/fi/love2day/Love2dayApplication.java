dbca7d9f13e02c967727217aedd8bdf0c2193a8f
suuniqo <suuniqo@gmail.com>
2026-05-20 19:46:49 +0200

    first commit

---
+++ src/main/java/es/upm/fi/love2day/Love2dayApplication.java
@@ -0,0 +1,13 @@
+package es.upm.fi.love2day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Love2dayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Love2dayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/fi/love2day/Love2dayApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.fi.love2day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Love2dayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
